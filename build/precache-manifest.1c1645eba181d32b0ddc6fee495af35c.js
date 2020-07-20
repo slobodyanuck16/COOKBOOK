0bacad35012048021286a231444d077e6674f105
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-07-20 14:22:31 +0300

    c

---
+++ build/precache-manifest.1c1645eba181d32b0ddc6fee495af35c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ae188b5087b78624576fae9bc12c13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be9bdafafad7cac5152f",
+    "url": "/static/css/main.d4fdd69a.chunk.css"
+  },
+  {
+    "revision": "36e3c143c61259e301b5",
+    "url": "/static/js/2.9b257ddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b257ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9bdafafad7cac5152f",
+    "url": "/static/js/main.7a26a69a.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
